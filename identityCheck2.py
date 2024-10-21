0829756410b98977bc7890502b8e2ff8cb79733c
Roo-M <51594@edu.raseko.fi>
2024-10-21 23:22:42 +0300

    Rakennettu metodit pituuden ja osien selvittämiseen

--- identityCheck2.py
+++ identityCheck2.py
@@ -25,4 +25,60 @@ class NationalSSN:
         self.gender = ''
         self.checkSum = ''
 
-# Luokan metodit eri osien laskentaan ja järkevyystarkistuksiin
+    # Luokan metodit eri osien laskentaan ja järkevyystarkistuksiin
+
+    # Tarkistetaan, että HeTu:n pituus on 11 merkkiä
+    def checkSsnLengthOk(self) -> bool:
+        ssnLength = len(self.ssn)
+        if ssnLength != 11:
+            return False
+            # TODO: Mieti pitäisikä tässä generoida virheilmoitus (raise)
+        else:
+            return True
+
+    # Pilkotaan henkilötunnus osiin
+    def splitSsn(self) -> dict:
+        """Splits the SSN to functional parts ie. birthdate, century, number and the checksum
+
+        Returns:
+            dict: parts as strings
+        """
+        # Tehdään pilkkominen vain jos pituus on oikein
+        if self.checkSsnLengthOk(): # Jos True pilkotaan, huom self.metodinNimi
+            dayPart = self.ssn[0:2]
+            monthPart = self.ssn[2:4]
+            yearPart = self.ssn[4:6]
+            centuryPart = self.ssn[6]
+            birthNumberPart = self.ssn[7:10]
+            checksumPart = self.ssn[10]
+            return {'days': dayPart,
+                    'months': monthPart,
+                    'years': yearPart,
+                    'century': centuryPart,
+                    'number':  birthNumberPart,
+                    'checksum': checksumPart
+                    }
+        else:
+            # TODO: Mieti, pitäisikö synnyttää virhetilanne raisella
+            return {'status' : 'error'}
+
+    # Muutetaan syntymäaikaosa ja vuosisata päivämääräksi
+    def getDateOfBirth(self, arg):
+        pass
+
+    # Lasketaan ikä nyt täysinä vuosina
+    def calculateAge(self, arg):
+        pass
+
+    # Selvitetään varmistussumman avulla onko HeTu syötetty oikein
+    def isValidSsn(self, arg):
+        pass
+
+
+# MAIN KOKEILUJA VARTEN (Poista, kun ei enää tarvita)
+# ===================================================
+
+if __name__ == "__main__":
+    hetu1 = NationalSSN('130728-478N')
+    print('Oikein muodostettu', hetu1.checkSsnLengthOk())
+    print('HeTu:n osat ovat: ', hetu1.splitSsn())

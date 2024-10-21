8c6f303d1bb3d55bfa77a4d53eaea6b1138a503b
Roo-M <51594@edu.raseko.fi>
2024-10-21 21:56:54 +0300

    LIsätty harjoitusosioon selityksiä

--- luokkaHarjoitus.py
+++ luokkaHarjoitus.py
@@ -4,15 +4,26 @@
 
 # Yliluokka (Parent class, mother class, superclass)
 class Henkilo:
+
+    # Konstruktori(metodi), jonka avulla luodaan uusi Henkilö-olio
+    # Oliota luotaessa etunimi ja sukunimi ovat pakollisia tietoja
     def __init__(self, etunimi, sukunimi):
         self.etunimi = etunimi
         self.sukunimi = sukunimi
 
+        # Oliolla on myös muita ominaisuuksia, joita ei määritellä olion luomisen yhteydessä
+        self.ika = 0 # Oletusikä 0
+        self.kaupunki = '' # Asuinkapunki tyhjä
+        self.harrastukset = [] # Harrastukset tyhjä lista
+
 # Aliluokka (Child class, daughter class subclass) perii (inherit) Henkilo-luokan 
 class Opiskelija(Henkilo):
-    def __init__(self, etunimi, sukunimi, ryhma):
-        super().__init__(etunimi, sukunimi)
-        self.ryhma = ryhma
+
+    # Oliota luotaessa pakollisia ovat etunimi ja sukunimi (koska pakollisia)
+    # Yliluokassa Henkilo sekä ryhmä
+    def __init__(self, etunimi, sukunimi, ryhma): # Metodi joka muodostaa opiskelija-olion
+        super().__init__(etunimi, sukunimi) # Kertoo, että yliluokassa on määritelty etunimen ja sukunimen käsittely
+        self.ryhma = ryhma # Tämän parametrin käsittelyä ei ole kerrottu yliluokassa
 
 # Aliluokka (Child class, daughter class subclass) perii (inherit) Henkilo-luokan
 class Opettaja(Henkilo):
@@ -30,13 +41,22 @@ if __name__ == "__main__":
 
     # Johdetaan (instantiate) luokasta Henkilo olio rehtori
     rehtori = Henkilo('Reijo', 'Rantanen')
+    rehtori.harrastukset = ['sulkapallo', 'rullaluistelu']
 
     # Luodaan olio opiskelija
     opiskelija = Opiskelija('Jakke', 'Jäynä', 'Tivi20oA')
+    opiskelija.harrastukset = ['kokkaaminen', 'punttisali']
 
     # Luodaan olio Oppivelvollinen-luokasta
     oppivelvollinen = Oppivelvollinen('Jonne', 'Janttari', 'Tivi24A', '2027-05-20')
+    oppivelvollinen.harrastukset = ['pelaaminen', 'velttoilu']
 
     print('Koulun rehtorina toimii', rehtori)
 
     print('Koulun rehtorina toimii', rehtori.etunimi, rehtori.sukunimi)
+
+    print('Rehtori harrastaa', rehtori.harrastukset)
+
+    print('Jakke Jäynä', opiskelija.harrastukset)
+
+    print('Jonne harrastaa', oppivelvollinen.harrastukset)

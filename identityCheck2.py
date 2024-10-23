eff9f2eab44b1f6faf407df4e74e6eca8a953832
Roo-M <51594@edu.raseko.fi>
2024-10-23 15:18:08 +0300

    Lisätty automaattisia yksikkötestejä

--- identityCheck2.py
+++ identityCheck2.py
@@ -26,7 +26,6 @@ class NationalSSN:
         self.dateOfBirth = ''
         self.number = 0
         self.gender = ''
-        self.checkSum = ''
 
         # Sanakirjat vuosisatakoodeille ja varmisteille
         self.centuryCodes = {
@@ -148,7 +147,7 @@ class NationalSSN:
             try:
                 century = self.centuryCodes[centurySymbol]
             except:
-                raise ValueError('Vuosisata merkki virheellinen')
+                raise ValueError('Vuosisatamerkki virheellinen')
 
             isoDate = century[0:2] + parts['years'] + \
                 '-' + parts['months'] + '-' + parts['days']
@@ -172,6 +171,26 @@ class NationalSSN:
             # Palautetaan ikä vuosina
             return ageInYears
 
+    # Metodi sukupuolen selvittämiseen
+    def getGender(self):
+
+        # Tarkistetaan ensin, onko SSN oikein syötetty
+        if self.isValidSsn():
+
+            # Otetaan merkkijonosta järjestysnumero hyödyntämällä splitSSn-metodia
+            parts = self.splitSsn()
+
+            # Mutetaan luvuksi ja tallennetaan se ominaisuuden arvoksi
+            number = int(parts['number'])
+            self.number = number
+
+            # Selvitetään onko parillinen (tyttö) vai pariton (poika)
+            if number % 2 == 0:
+                self.gender = 'Nainen'
+            else:
+                self.gender = 'Mies'
+
+
 # MAIN KOKEILUJA VARTEN (Poista, kun ei enää tarvita)
 # ===================================================
 
@@ -179,13 +198,17 @@ if __name__ == "__main__":
     try:
         hetu1 = NationalSSN('130728-478N')
         hetu1.checkSsnLengthOk()
-        hetu1.calculateAge()
+        hetu1.getDateOfBirth()
+        # ika = hetu1.calculateAge()
+        # hetu1.getGender()
     except Exception as e:
         print('Tapahtui virhe:', e)
 
-    # ika = hetu1.calculateAge()
-    # print('Oikein muodostettu:', hetu1.checkSsnLengthOk())
-    # print('HeTun osat ovat: ', hetu1.splitSsn())
-    # print('Syntymäaikaosa ISO-muodossa on ', hetu1.dateOfBirth)
-    # print('Henkilötunnus on oikein muodostettu', hetu1.isValidSsn())
+
+    print('On oikean pituinen:', hetu1.checkSsnLengthOk())
+    print('Henkilötunnus on oikein muodostettu', hetu1.isValidSsn())
+    print('HeTun osat ovat: ', hetu1.splitSsn())
+    print('Syntymäaikaosa ISO-muodossa on ', hetu1.dateOfBirth)
+  
     # print('Henkilön ikä on', ika)
+    # print('Ja hän on ', hetu1.gender)

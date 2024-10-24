856366f25e44fe71d0a333b6841dcc2c85af8914
Roo-M <51594@edu.raseko.fi>
2024-10-24 14:59:00 +0300

    Tehty input kysymykset ja varnistuksia ettei ohjelma kaadu syöttövirheeseen

--- app.py
+++ app.py
@@ -7,19 +7,42 @@
 # MODUULIT
 # --------
 
-import sound # Äänimerkit ja äänitiedostot
-import video # Videomoduuli
+from avtools import sound # Äänimerkit ja äänitiedostot
+from avtools import video # Videomoduuli
+import identityCheck2
 
 # ASETUKSET
 # ---------
 kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0
 
-# Käynnistetään videokuva ja ilmoitetaan sen käynnistymisestä äänimerkillä
-sound.parametricBeep(400,330)
-sound.playWav('Alkaa.WAV')
+# TODO: Tee tarkistus siitä että nimi ei voi olla tyhjä
 
-# TESTIT KOODAUKSEN AIKANA
-# ========================
+# TODO: Rakenna funktio, jolla kysytään nimet ja muutetaan yhdysnimet isoille alkukirjaimille -> reg exp
 
-if __name__ == "__main__":
-    pass
+while True:
+    userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
+    userGivenSsn = userGivenSsn.upper() # Varmistetaan, että tarkiste on isolla
+
+    ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
+    if ssnToCheck.isValidSsn() == True:
+        try:
+            ssnToCheck.getDateOfBirth()
+            ssnToCheck.getGender()
+            age = ssnToCheck.calculateAge()
+            userGivenLastName = input('Syötä asikkaan sukunimi: ')
+            userGivenLastName = userGivenLastName.capitalize()
+            userGivenFirstName = input('Syötä asiakkaan etunimi: ')
+            userGivenFirstName = userGivenFirstName.capitalize()
+            print('Asiakkas: ', userGivenLastName, userGivenFirstName)
+            print('Syntymäaika:', ssnToCheck.dateOfBirth)
+            print('Ikä:', age)
+            print('Sukupuoli:', ssnToCheck.gender)
+        except Exception as e:
+            print('Syöttämässäsi sosiaaliturvatunnuksessa oli virhe:', e)
+
+    # Kysytään halutaanko poistua ohjelmasta
+    wantExit = input('Haluatko päättää ohjelman? Vastaa k/E: ')
+    # Muutetaan vastaus isoiksi kirjaimiksi ja tarkistetaan onko se K
+    if wantExit.upper == 'K':
+        break # Poistutaan ikuisesta silmukasta
+

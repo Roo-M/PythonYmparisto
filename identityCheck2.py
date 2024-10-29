4509d13f2dcc1d79f116925d6c18d381c189b64e
Roo-M <51594@edu.raseko.fi>
2024-10-29 09:31:14 +0200

    Lisätty kommenttia

--- identityCheck2.py
+++ identityCheck2.py
@@ -109,6 +109,7 @@ class NationalSSN:
                     'number':  birthNumberPart,
                     'checksum': checksumPart
                     }
+        # Else haaran tarkoitus on vain estää PyLance-virhe, ei palauta oikeasti mitään, vaan antaa virheilmoituksen, jos HeTu väärän mittainen
         else:
             return {'status' : 'error'}
         

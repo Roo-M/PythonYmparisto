23d25274ded61d3a53d987f3e33f86191553289f
Roo-M <51594@edu.raseko.fi>
2024-10-30 16:19:35 +0200

    Päivitetty testejä ja siirretty ne omaan kansioon

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,6 +7,7 @@ import app
 # Testitapaus 1 pienellä kirjoitettu nimi tulee olla isolla
 def test_smallInput(monkeypatch):
     simulatedInput = 'anna-liisa' # Simuloitu käyttäjän syöte
+    
     # Lähetetään simuloitu syöte monkeypatch:n avulla funktiolle
     monkeypatch.setattr('builtins.input', lambda _: simulatedInput)
     assert app.askName('Nimi') == 'Anna-Liisa'

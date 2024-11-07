0251e804249bfc3eb1da9cc8e6696da50fc65efc
Roo-M <51594@edu.raseko.fi>
2024-11-07 18:58:53 +0200

    korjattu viimeisen testin assert

--- tests/test_barcode.py
+++ tests/test_barcode.py
@@ -114,5 +114,5 @@ def test_defaultVariant():
 
 # Testataan oikein muodostetun viivakoodin syntyminen
 def test_validBarcode():
-    assert barcode1.buildBarcode() == 'Ì128B_Î'
+    assert barcode1.buildBarcode() == 'Ì128BXÎ'
 
